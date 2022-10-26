b914270b31a053de51bb045a94419c4a37933c05
horsellcommon <crawfordj716@gmail.com>
2022-10-26 15:57:11 +0100

    middleware completed

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -11,3 +11,34 @@ exports.hashPass = async (request, response, next) => {
     response.status(500).send({ error: error.message });
   }
 };
+
+exports.tokenCheck = async (request, response, next) => {
+  try {
+    const token = request.header("Authorization");
+    const decodedToken = await jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(decodedToken._id);
+    request.user = user;
+    console.log(user);
+    next();
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ error: error.message });
+  }
+};
+
+exports.comparePasswords = async (request, response, next) => {
+  try {
+    request.user = await User.findOne({ username: request.body.username });
+    if (
+      request.user &&
+      (await bcrypt.compare(request.body.password, request.user.password))
+    ) {
+      next();
+    } else {
+      throw new Error("Incorrect userid or password...");
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).send({ error: error.message });
+  }
+};
