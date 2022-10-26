7009e5cc34b7a95119774381c5ba06906e41d9a0
horsellcommon <crawfordj716@gmail.com>
2022-10-26 15:29:15 +0100

    connection.js file complete

---
+++ src/db/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connection successful...");
+  } catch (error) {
+    console.log(`Could not connect...` + error);
+  }
+};
