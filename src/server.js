553cc1356fd9710ac75a5102c63546a69e5aaf28
horsellcommon <crawfordj716@gmail.com>
2022-10-26 15:44:45 +0100

    userRouter completed - added unfinished middleware and added userController requirements

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("./db/connection");
+const userRouter = require("./user/userRouters");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(userRouter);
 
 app.listen(port, () => {
   console.log(`Success! Listening on port ${port}`);
---
+++ src/user/userRouters.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const {
+  createUser,
+  readUsers,
+  updateUser,
+  deleteUser,
+  loginUser,
+} = require("./userController");
+const { hashPass, tokenCheck, comparePass } = require("../middleware");
+
+const userRouter = Router();
+
+userRouter.post("/create", hashPass, createUser);
+userRouter.post("/login", comparePass, tokenCheck, loginUser);
+userRouter.get("/read", readUsers);
+userRouter.put("/update", updateUser);
+userRouter.delete("/delete/:username", deleteUser);
