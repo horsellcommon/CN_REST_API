a0b16d5d222ab300aefb5daf5b5b71e07905a5c6
horsellcommon <crawfordj716@gmail.com>
2022-10-27 13:43:08 +0100

    Updated server.js to include dataRouter

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 require("./db/connection");
 const userRouter = require("./user/userRouters");
 const express = require("express");
+const dataRouter = require("./userdata/dataRouters");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(userRouter);
+app.use(userRouter, dataRouter);
 
 app.listen(port, () => {
   console.log(`Success! Listening on port ${port}`);
