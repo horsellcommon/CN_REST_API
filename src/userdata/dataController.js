a0d59f2c8d70390e3fb9d3814dd356617156aac2
horsellcommon <crawfordj716@gmail.com>
2022-10-27 14:04:43 +0100

    Added if statement, commented bugs that need fixing in various files

--- src/userdata/dataController.js
+++ src/userdata/dataController.js
@@ -1,4 +1,5 @@
-const Data = require("../userdata/dataModel"); // Import user for lines 15/16?
+const Data = require("./dataModel"); // Import user for lines 15/16?
+const User = require("../user/userModel");
 
 exports.createData = async (request, response) => {
   try {
@@ -12,8 +13,14 @@ exports.createData = async (request, response) => {
 
 exports.readData = async (request, response) => {
   try {
-    const info = await Data.find({}); // if username matches User then return both Data/User objects
-    response.status(200).send({ data: info });
+    const info = await Data.find({});
+    const userdata = await User.find({});
+    if (response.data.username == User.data.username) { // username Undefined
+      response.status(200).send({ data: info, userdata });
+    } else {
+      console.log("Unable to find, showing only date of birth data.");
+      response.status(200).send({ data: info });
+    }
   } catch (error) {
     console.log(error);
     response.status(500).send({ error: error.message });
@@ -24,7 +31,7 @@ exports.updateData = async (request, response) => {
   try {
     await Data.updateOne(
       { username: request.body.username }, // Might not work???
-      { [request.body.key]: request.body.value }
+      { [request.body.key]: request.body.value } // Doesnt work lol
     );
     response.status(201).send({ message: "Record successfully updated." });
   } catch (error) {
@@ -34,6 +41,7 @@ exports.updateData = async (request, response) => {
 };
 
 exports.deleteData = async (request, response) => {
+  // Also doesn't work?
   try {
     await Data.deleteOne({ dateofbirth: request.params.dateofbirth });
     response.status(200).send({ message: "Data successfully deleted." });
